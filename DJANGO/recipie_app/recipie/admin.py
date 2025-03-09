6f23fcd20dd468ddc0e2c847271cdbd72743d1a1
Samundra Poudel <samundra1614.be23@chitkarauniversity.edu.in>
2025-03-09 17:50:30 +0530

    Student Report Card

--- DJANGO/recipie_app/recipie/admin.py
+++ DJANGO/recipie_app/recipie/admin.py
@@ -7,3 +7,6 @@ admin.site.register(Recipie)
 admin.site.register(Department)
 admin.site.register(StudentID)
 admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(SubjectMarks)
+

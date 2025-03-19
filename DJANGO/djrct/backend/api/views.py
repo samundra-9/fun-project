d248c264f83d65d69d5474c01b59ed7ae4c8f722
Samundra Poudel <samundra1614.be23@chitkarauniversity.edu.in>
2025-03-19 22:50:31 +0530

    dependencis and al

---
+++ DJANGO/djrct/backend/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+# Create your views here.
